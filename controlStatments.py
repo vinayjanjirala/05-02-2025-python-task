ce73a20c7f7631f0eaa6e0611a83a098bbcb009b
vinayjanjirala <vinayjanjirala7@gmail.com>
2025-02-05 22:19:34 +0530

    if else

--- controlStatments.py
+++ controlStatments.py
@@ -1,9 +1,7 @@
-# fighter1 = 20
-# fighter2 = 30
 attack1 = int(input("Give attack power for 1st figher: "))
 attack2 = int(input("Give attack power for 2nd figher: "))
 health1 = int(input("Give health for 1st figher: "))
-health2 = int(input("Give health for 2nd2 figher: "))
+health2 = int(input("Give health for 2nd figher: "))
 
 if attack1>attack2:
     print("fighter 1 wins")
